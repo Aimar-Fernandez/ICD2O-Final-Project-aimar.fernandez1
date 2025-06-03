27b3c7aeee27f99be44af5898d4e506deb1ac227
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-06-03 17:30:45 +0000

    added body expansion

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,11 +10,16 @@ class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'gameScene' })
     this.background = null
+    this.leafImage = null
     this.leaf = null
     this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: '65px Arial', fill: '#000000', align: 'center' }
     this.wormHead = null
     this.wormBody2 = null
     this.wormBody3 = null
+    this.wormBody4 = null
+    this.wormBody5 = null
     this.direction = 0
     this.lastMovement = 0
   }
@@ -26,6 +31,7 @@ class GameScene extends Phaser.Scene {
   preload () {
     console.log('Game Scene')
     // images
+    this.load.image('background', './assets/background.png')
     this.load.image('wormHead', './assets/wormHead.png')
     this.load.image('wormBody2', './assets/darkGreenSqaure.png')
     this.load.image('wormBody3', './assets/wormHead.png')
@@ -33,10 +39,16 @@ class GameScene extends Phaser.Scene {
   }
 
   create (data) {
+    // this.background = this.add.image(1920 / 2 ,1080 / 2, 'background').setScale(1.0)
+    this.leafImage = this.add.image(10, 10, 'leaf').setScale(0.15)
+    this.leafImage.setOrigin(0, 0)
     this.wormHead = this.physics.add.sprite(1920 / 2, 1080 / 2, 'wormHead').setScale(0.10)
+    //sprites
     this.wormBody2 = this.physics.add.sprite(1920 / 2 - 60, 1080 / 2, 'wormBody2').setScale(0.10)
     this.wormBody3 = this.physics.add.sprite(1920 / 2 - 120, 1080 / 2, 'wormBody3').setScale(0.10)
     this.leaf = this.physics.add.sprite(1920 / 2 + 240, 1080 / 2, 'leaf').setScale(0.10)
+    // text
+    this.scoreText = this.add.text(10, 10, this.score.toString(), this.scoreTextStyle)
   }
 
   update (time, delta) {
@@ -57,6 +69,38 @@ class GameScene extends Phaser.Scene {
       // Do nothing
     }
     if (time > this.lastMovement + 250) {
+      // worm body 5
+      if (this.wormBody5 != null) {
+        if (this.wormBody5.x > this.wormBody4.x) {
+          this.wormBody5.x -= 60
+        } else if (this.wormBody5.x < this.wormBody4.x) {
+          this.wormBody5.x += 60
+        } else {
+          if (this.wormBody5.y > this.wormBody4.y) {
+            this.wormBody5.y -= 60
+          } else if (this.wormBody5.y < this.wormBody4.y) {
+            this.wormBody5.y += 60
+          } else {
+            // do nothing
+          }
+        }
+      }
+      // worm body 4
+      if (this.wormBody4 != null) {
+        if (this.wormBody4.x > this.wormBody3.x) {
+          this.wormBody4.x -= 60
+        } else if (this.wormBody4.x < this.wormBody3.x) {
+          this.wormBody4.x += 60
+        } else {
+          if (this.wormBody4.y > this.wormBody3.y) {
+            this.wormBody4.y -= 60
+          } else if (this.wormBody4.y < this.wormBody3.y) {
+            this.wormBody4.y += 60
+          } else {
+            // do nothing
+          }
+        }
+      }
       // worm body 3
       if (this.wormBody3.x > this.wormBody2.x) {
         this.wormBody3.x -= 60
@@ -99,10 +143,18 @@ class GameScene extends Phaser.Scene {
       }
       if (this.wormHead.x === this.leaf.x && this.wormHead.y === this.leaf.y) {
         while (this.leaf.x === this.wormHead.x && this.leaf.y === this.wormHead.y) {
-          this.leaf.x = Math.floor(Math.floor(Math.random() * 1200 + 1) / 60) * 60 + 360
+          this.leaf.x = Math.floor(Math.floor(Math.random() * 960 + 1) / 60) * 60 + 480
           this.leaf.y = Math.floor(Math.floor(Math.random() * 960 + 1) / 60) * 60 + 60
         }
         this.score += 1
+        this.scoreText.setText(this.score.toString())
+        if (this.wormBody4 === null) {
+          this.wormBody4 = this.physics.add.sprite(this.wormBody3.x, this.wormBody3.y, 'wormBody2').setScale(0.10)
+        } else if (this.wormBody5 === null) {
+          this.wormBody5 = this.physics.add.sprite(this.wormBody4.x, this.wormBody4.y, 'wormBody3').setScale(0.10)
+        } else {
+          // empty for now
+        }
       }
       this.lastMovement = time
     }
